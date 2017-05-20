448989fad828325dc6a0b8222dff10e7c8d76b57
Joshua Buchacher <jbuchacher@me.com>
2017-05-19 20:01:02 -0700

    Render question mark

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 const sense = require("node-sense-hat").Leds;
 
-sense.setPixel(0, 7, [244,0,0], (err) => {
-  sense.getPixel(0, 7, (err, color) => {
-    console.log(color);
-  }
-});
+var X = [255, 0, 0];  // Red
+var O = [255, 255, 255];  // White
+
+var questionMark = [
+O, O, O, X, X, O, O, O,
+O, O, X, O, O, X, O, O,
+O, O, O, O, O, X, O, O,
+O, O, O, O, X, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, X, O, O, O, O
+];
+
+sense.setPixels(questionMark);
